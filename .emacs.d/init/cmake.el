86cbc4f1ca8c0b9a549df1be119c7ac4ec34b5fc
Alastair Rankine <alastair@girtby.net>
2013-01-14 14:46:19 -0600

    Use SubWord mode in cmake

--- .emacs.d/init/cmake.el
+++ .emacs.d/init/cmake.el
@@ -2,4 +2,8 @@
 (require 'cmake-mode)
 (setq cmake-tab-width 4)
 
+(when (fboundp 'subword-mode)
+  (add-hook 'cmake-mode-hook (lambda ()
+                               (subword-mode 1))))
+
 (provide 'init/cmake)
