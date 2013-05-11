a5e51c64ffcd48f40d44f5261e5eeaf7e4f3d3b9
Alastair Rankine <alastair@girtby.net>
2013-05-11 13:21:33 -0500

    bootstrap: Add function wrappers

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,70 +5,78 @@ shopt -s nocasematch nullglob    # using Bash
 dotfiles=${0%%/*}
 dotfiles_abs=$(cd $dotfiles && pwd -L)
 
-echo "** checking environment"
+check_environment() {
+    echo "** checking environment"
 
-required_exes=(git bzr make)
-
-for e in ${required_exe[@]}; do
-    hash $e || {
-        echo "!! Missing: $e"
+    required_exes=(git bzr make)
+    
+    for e in ${required_exe[@]}; do
+        hash $e || {
+            echo "!! Missing: $e"
+            exit 1
+        }
+    done
+    
+    if (( BASH_VERSINFO[0] < 4 )); then
+        echo "!! Bash version < 4: $BASH_VERSION"
         exit 1
-    }
-done
-
-if (( BASH_VERSINFO[0] < 4 )); then
-    echo "!! Bash version < 4: $BASH_VERSION"
-    exit 1
-fi
-
-echo "** setting up git config"
-
-# Don't use work email name/address for this repo...
-git config --file "$dotfiles/.git/config" user.name "Alastair Rankine"
-git config --file "$dotfiles/.git/config" user.email "alastair@girtby.net"
-
-
-echo "** setting up symlinks to dotfiles in: $dotfiles_abs"
-
-skipfiles=(bootstrap.sh)
-
-case "$(uname -s)" in
-    "Linux"   )   statinode() { stat -c '%i' "$@"; return 0; };;
-    "Darwin"  )   statinode() { stat -L -f '%i' "$@"; return 0; };;
-    "FreeBSD" )   statinode() { stat -L -f '%i' "$@"; return 0; };;
-    * )           statinode() { ls -id "$@" | cut -d ' ' -f 1; return 0; };;
-esac
-
-for dst in $dotfiles/* ; do
-    nm=${dst##*/}
-
-    # TODO: is this a reliable test?
-    if [[ ${skipfiles[*]} =~ $nm ]]; then
-        continue
     fi
+}
+check_environment
 
-    src=$HOME/.$nm
+git_config() {
+    echo "** setting up git config"
 
-    if [[ -e $src ]]; then
-        if [[ -L $src ]]; then
-            linkinode=$(statinode $src)
-            dstinode=$(statinode $dst)
-            if (( linkinode != dstinode )); then
-                echo "warning: $src is a symlink but doesn't point to desired $dst ($linkinode != $dstinode)"
-            fi
-        else
-            echo "warning: $src exists, cannot create symlink"
+    # Don't use work email name/address for this repo...
+    git config --file "$dotfiles/.git/config" user.name "Alastair Rankine"
+    git config --file "$dotfiles/.git/config" user.email "alastair@girtby.net"
+}
+git_config
+
+symlink_dotfiles() {
+    echo "** setting up symlinks to dotfiles in: $dotfiles_abs"
+    
+    skipfiles=(bootstrap.sh)
+    
+    case "$(uname -s)" in
+        "Linux"   )   statinode() { stat -c '%i' "$@"; return 0; };;
+        "Darwin"  )   statinode() { stat -L -f '%i' "$@"; return 0; };;
+        "FreeBSD" )   statinode() { stat -L -f '%i' "$@"; return 0; };;
+        * )           statinode() { ls -id "$@" | cut -d ' ' -f 1; return 0; };;
+    esac
+    
+    for dst in $dotfiles/* ; do
+        nm=${dst##*/}
+        
+        # TODO: is this a reliable test?
+        if [[ ${skipfiles[*]} =~ $nm ]]; then
+            continue
         fi
         
-        continue
-    fi
-
-    echo "$src -> $dst"
-    (
-        cd $HOME
-        ln -s ${dotfiles_abs#$HOME/}/$nm .$nm
-    )
-done
+        src=$HOME/.$nm
+        
+        if [[ -e $src ]]; then
+            if [[ -L $src ]]; then
+                linkinode=$(statinode $src)
+                dstinode=$(statinode $dst)
+                if (( linkinode != dstinode )); then
+                    echo "warning: $src is a symlink but doesn't point to desired $dst ($linkinode != $dstinode)"
+                fi
+            else
+                echo "warning: $src exists, cannot create symlink"
+            fi
+            
+            continue
+        fi
+        
+        echo "$src -> $dst"
+        (
+            cd $HOME
+            ln -s ${dotfiles_abs#$HOME/}/$nm .$nm
+        )
+    done
+}
+symlink_dotfiles
 
 checkout_libs() {
     echo "** Checking out bzr libs"
