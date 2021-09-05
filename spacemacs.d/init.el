d999bb6c733bbbe158612d4205e68de500d6623e
Alastair Rankine <alastair@girtby.net>
2021-09-05 14:41:56 +1000

    spacemacs.d/init.el: use spacemacs-dark theme by default

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -236,10 +236,10 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(zenburn
-                         hc-zenburn
-                         spacemacs-dark
+   dotspacemacs-themes '(spacemacs-dark
                          spacemacs-light
+                         zenburn
+                         hc-zenburn
                          doom-one)
 
    ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
