e27f790060eb1c941b4bda5eafba628548f6d71f
Alastair Rankine <alastair@girtby.net>
2023-01-05 14:59:27 -0500

    custom-post: move company-yasnippet to C-c y

--- custom-post.el
+++ custom-post.el
@@ -1,1 +1,7 @@
 ;;; custom-post.el --- user customization file    -*- lexical-binding: t no-byte-compile: t -*-
+
+;; company-yasnippet is far too noisy - just stick to company-capf for now
+(setcar company-backends 'company-capf)
+
+;; use C-c y for yasnippet instead (previously bound to my-youdao-dictionary-search-at-point, already accessible from C-c d Y)
+(bind-key "C-c y" 'company-yasnippet)
