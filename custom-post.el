b623a3a45a4a6b2163a6f9f4684ea3f21fde810f
Alastair Rankine <alastair@girtby.net>
2023-02-08 10:17:03 -0500

    custom-post: unbind M-digit keys from magit-section-mode

--- custom-post.el
+++ custom-post.el
@@ -5,3 +5,11 @@
 
 ;; use C-c y for yasnippet instead (previously bound to my-youdao-dictionary-search-at-point, already accessible from C-c d Y)
 (bind-key "C-c y" 'company-yasnippet)
+
+;; unbind these from magit mode, used for window switching
+(with-eval-after-load 'magit-status
+  (unbind-key "M-1" magit-section-mode-map)
+  (unbind-key "M-2" magit-section-mode-map)
+  (unbind-key "M-3" magit-section-mode-map)
+  (unbind-key "M-4" magit-section-mode-map)
+  )
